22b30d8803203c86f920bfd14cad5535f88aa34b
beatriizsilva <134173065+beatriizsilva@users.noreply.github.com>
2024-10-24 23:56:06 -0300

    Update laudoController.js

--- src/controllers/laudoController.js
+++ src/controllers/laudoController.js
@@ -18,16 +18,16 @@ exports.createLaudo = async (req, res) => {
 };
 
 // Listar todos os laudos (máximo 50)
-exports.getAllLaudos = async (req, res) => {
-  try {
-    // TODO pensar em um modo de paginação
-    // -> Retorna total de elementos, total de páginas, página atual e total de elementos na página
-    const laudos = await Laudo.find().limit(50);
-    res.status(200).json(laudos);
-  } catch (error) {
-    res.status(500).send({ message: 'Erro ao listar laudos.', error });
-  }
-};
+// exports.getAllLaudos = async (req, res) => {
+//   try {
+//     // TODO pensar em um modo de paginação
+//     // -> Retorna total de elementos, total de páginas, página atual e total de elementos na página
+//     const laudos = await Laudo.find().limit(50);
+//     res.status(200).json(laudos);
+//   } catch (error) {
+//     res.status(500).send({ message: 'Erro ao listar laudos.', error });
+//   }
+// };
 
 // Buscar laudo por ID
 exports.getLaudoById = async (req, res) => {
@@ -43,6 +43,49 @@ exports.getLaudoById = async (req, res) => {
     res.status(500).send({ message: 'Erro ao buscar o laudo.', error });
   }
 };
+
+// Buscar laudos por termo
+// exports.searchLaudos = async (req, res) => {
+//   const { t } = req.query;
+//   if (!t) {
+//     return res.status(400).send({ message: 'Termo de busca é obrigatório.' });
+//   }
+
+//   try {
+//     const laudos = await Laudo.find({ texto: { $regex: t, $options: 'i' } }).limit(50);
+//     res.status(200).json(laudos);
+//   } catch (error) {
+//     res.status(500).send({ message: 'Erro ao buscar laudos.', error });
+//   }
+// };
+// Função para listar todos os laudos ou buscar pelo termo `t`
+exports.searchOrListLaudos = async (req, res) => {
+  const { t } = req.query;  // Captura o parâmetro `t`
+
+  try {
+    let laudos;
+
+    // Verifica se o parâmetro `t` foi informado mas está vazio
+    if (t === "") {
+      return res.status(400).send({ message: 'Termo de busca não pode estar vazio.' });
+    }
+
+    // Se `t` foi passado, faz a busca no campo `texto`
+    if (t) {
+      console.log('Buscando laudos com o termo:', t);
+      laudos = await Laudo.find({ texto: { $regex: t, $options: 'i' } }).limit(50);
+    } else {
+      // Se `t` não foi passado, lista todos os laudos
+      console.log('Listando todos os laudos');
+      laudos = await Laudo.find().limit(50);  // Ajuste o limite conforme necessário
+    }
+
+    res.status(200).json(laudos);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao buscar ou listar laudos.', error });
+  }
+};
+
 
 // Buscar laudos por termo
 exports.searchLaudos = async (req, res) => {
