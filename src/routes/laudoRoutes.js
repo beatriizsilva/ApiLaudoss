3be81ca2b5c1c97afcbca8a9aaccbd33a5477845
beatriizsilva <134173065+beatriizsilva@users.noreply.github.com>
2024-10-24 23:59:37 -0300

    Update laudoRoutes.js

--- src/routes/laudoRoutes.js
+++ src/routes/laudoRoutes.js
@@ -1,11 +1,26 @@
+// const express = require('express');
+// const laudoController = require('../controllers/laudoController');
+
+// const router = express.Router();
+
+// router.post('/laudos', laudoController.createLaudo);
+// router.get('/laudos/:id', laudoController.getLaudoById);
+// router.get('/laudos', laudoController.searchLaudos);
+
+// module.exports = router;
 const express = require('express');
 const laudoController = require('../controllers/laudoController');
 
 const router = express.Router();
 
-router.post('/laudos', laudoController.createLaudo);
-router.get('/laudos', laudoController.getAllLaudos);
-router.get('/laudos/:id', laudoController.getLaudoById);
-router.get('/laudos/search', laudoController.searchLaudos);
+// Rota para listar todos os laudos ou buscar com `t`
+router.get('/laudos', laudoController.searchOrListLaudos);
+
+router.post('/laudos', laudoController.createLaudo);  // Criar laudo
+router.get('/laudos/:id', laudoController.getLaudoById);  // Buscar laudo por ID
 
 module.exports = router;
+
+
+
+
